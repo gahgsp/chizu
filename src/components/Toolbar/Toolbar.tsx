46e7462881ed4813c324762173d87df01deff441
GusRiffel <gustavoriffell@gmail.com>
2024-05-24 12:59:07 +0100

    Working on ToolBar CSS

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -4,14 +4,39 @@ import "./ToolbarStyles.css";
 const Toolbar = () => {
   return (
     <div className="ToolbarFrame">
-      <div>
-        <button>Polygons</button>
+      <div className="FirstPanel">
+        <div>
+          <h4>Add</h4>
+        </div>
+        <div>
+          <button>Point</button>
+        </div>
+        <div>
+          <button>Rectangle</button>
+        </div>
+        <div>
+          <button>Polygon</button>
+        </div>
+        <div>
+          <p>____</p>
+        </div>
+        <div>
+          <button>GeoJson</button>
+        </div>
       </div>
-      <div>
-        <button>POI</button>
-      </div>
-      <div>
-        <button>Fav</button>
+      <div className="SecondPanel">
+        <div>
+          <h4>Edit</h4>
+        </div>
+        <div>
+          <button>Undo</button>
+        </div>
+        <div>
+          <button>Delete</button>
+        </div>
+        <div>
+          <button>Confirm</button>
+        </div>
       </div>
     </div>
   );
